1f642676fb523863a7cbbcea449e50e243c02744
forresti <fiandola@gmail.com>
2013-07-26 11:26:28 -0700

    refactored to *not* use batch mode, for simplicity. my real application does use batch mode AND streams, though.

--- cufft_multiple_streams/main.cu
+++ cufft_multiple_streams/main.cu
@@ -7,44 +7,40 @@
 using namespace std;
 
 void cufftForward_dpmData(){
-    int howMany = 2;
-    int nRows = 4;
-    int nCols = 4;
 
-    int n[2] = {nRows, nCols};
-    cufftHandle forwardPlan; 
-    float* d_in; cufftComplex* d_freq; 
+    //note: in my 'real' app, I'm already using batch mode. 
+    // I need to do multiple batch mode operations, each of which operates on a different chunk of data. 
+    // hence, the need for doing cuFFT calls in streams
 
-    CHECK_CUFFT(cufftPlanMany(&forwardPlan,
-                  2, //rank
-                  n, //dimensions = {nRows, nCols}
-                  0, //inembed
-                  howMany, //istride
-                  1, //idist
-                  0, //onembed
-                  howMany, //ostride
-                  1, //odist
-                  CUFFT_R2C, //cufftType
-                  howMany /*batch*/));
+    int nRows = 16; //each FFT is tiny; doesn't saturate the GPU
+    int nCols = 16;
+    int number_of_FFTs = 3;
 
-    float* h_in = (float*)malloc(sizeof(float) * nRows*nCols*howMany);
-    for(int i=0; i<(nRows*nCols*howMany); i++){
-        h_in[i] = (float)i;
-        printf("h_in[%d] = %f \n", i, h_in[i]);
+    vector<cufftHandle> forwardPlan(number_of_FFTs);
+    vector<float*> d_in(number_of_FFTs);
+    vector<cufftComplex*> d_freq(number_of_FFTs);
+
+    int nStreams = number_of_FFTs;
+    vector<cudaStream_t> streams(nStreams);
+    for(int s=0; s<nStreams; s++){
+        cudaStreamCreate(&streams[s]);
     }
 
-    cufftComplex* h_freq = (cufftComplex*)malloc(sizeof(cufftComplex)*nRows*nCols*howMany);
-    CHECK_CUDART(cudaMalloc(&d_in, sizeof(float)*nRows*nCols*howMany));
-    CHECK_CUDART(cudaMemcpy(d_in, h_in, sizeof(float)*nRows*nCols*howMany, cudaMemcpyHostToDevice));
-    CHECK_CUDART(cudaMalloc(&d_freq, sizeof(cufftComplex)*nRows*nCols*howMany));
-    CHECK_CUDART(cudaMemset(d_freq, 0, sizeof(cufftComplex)*nRows*nCols*howMany));
 
-    CHECK_CUFFT(cufftExecR2C(forwardPlan, d_in, d_freq));
-
-    CHECK_CUDART(cudaMemcpy(h_freq, d_freq, sizeof(cufftComplex)*nRows*nCols*howMany, cudaMemcpyDeviceToHost));
-    for(int i=0; i<(nRows*nCols*howMany); i++){ 
-        printf("cufft h_freq[%d].(x,y) = %f,%f \n", i, h_freq[i].x, h_freq[i].y);
+    for(int i=0; i<number_of_FFTs; i++){
+        CHECK_CUFFT(cufftPlan2d(&forwardPlan[i], nCols, nRows, CUFFT_R2C));
+        CHECK_CUDART(cudaMalloc(&d_in[i], sizeof(float)*nRows*nCols));
+        CHECK_CUDART(cudaMemset(d_in[i], 0, sizeof(float)*nRows*nCols));
+        d_freq[i] = reinterpret_cast<cufftComplex *>(d_in[i]);
     }
+
+    double start = read_timer();
+    for(int i=0; i<number_of_FFTs; i++){
+        CHECK_CUFFT(cufftExecR2C(forwardPlan[i], d_in[i], d_freq[i]));
+    }
+    double forwardTime = read_timer() - start;
+    printf("time for %d forward FFTs in streams = %f \n", number_of_FFTs, forwardTime);
+    printf("avg time per FFT = %f \n", forwardTime/number_of_FFTs);
 }
 
 void deviceStuff(){
