fd13d69d5f03e61ddf3658cd79501d1c6884c83b
forresti <fiandola@gmail.com>
2013-10-12 13:56:07 -0700

    segfault...debugging

--- gradient_cpp/main.cpp
+++ gradient_cpp/main.cpp
@@ -14,28 +14,31 @@ static inline int clamp(int idx, int min_idx, int max_idx){
 void gradient_reference(Mat img){
     int width = img.cols;
     int height = img.rows;
-   
-    Mat gradX(height, width, 3, CV_32FC3);
-    Mat gradY(height, width, 3, CV_32FC3);
 
-#if 0
+    img.convertTo(img, CV_32FC3); //silly to promote to float, but this is just a sanity check impl. 
+    Mat gradX(width, height, 3, CV_32FC3);
+    Mat gradY(width, height, 3, CV_32FC3);
+
     for(int y=0; y<height; y++){
-        for(int x=0; x<width; x++)}
+        for(int x=0; x<width; x++){
             for(int channel=0; channel<3; channel++){
-                float gradX = img.at<cv::Vec3b>(y,x+1)[channel] - img.at<cv::Vec3b>(y,x-1)[channel];
-                float tmp_gradY = img.at<cv::Vec3b>(y+1,x)[channel] - img.at<cv::Vec3b>(y-1,x)[channel];
+                gradX.at<cv::Vec3f>(y,x)[channel] = img.at<cv::Vec3f>(y,x)[channel]; //test
+                //gradX.at<cv::Vec3f>(y,x)[channel] = fabs(img.at<cv::Vec3f>(y, clamp(x+1, 0, width-1))[channel] - 
+                //                                         img.at<cv::Vec3f>(y, clamp(x-1, 0, width-1))[channel]); 
+                //float tmp_gradY = img.at<cv::Vec3f>(y+1,x)[channel] - img.at<cv::Vec3f>(y-1,x)[channel];
 
             }
         }
     }
-#endif
+
+    imwrite("gradX_gold.jpg", gradX);
 }
 
 
 int main (int argc, char **argv)
 {
     Mat img = imread("../Lena.jpg");
-
+    gradient_reference(img);
 
     return 0;
 }
