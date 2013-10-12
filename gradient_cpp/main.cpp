2d4e56e6c397d79b27531e691238997137f5113e
forresti <fiandola@gmail.com>
2013-10-12 14:03:22 -0700

    ref impl makes sense

--- gradient_cpp/main.cpp
+++ gradient_cpp/main.cpp
@@ -16,24 +16,23 @@ void gradient_reference(Mat img){
     int height = img.rows;
 
     img.convertTo(img, CV_32FC3); //silly to promote to float, but this is just a sanity check impl. 
-    Mat gradX(width, height, CV_32FC3);
-    Mat gradY(width, height, CV_32FC3);
+    Mat gradX(height, width, CV_32FC3);
+    Mat gradY(height, width, CV_32FC3);
 
     for(int y=0; y<height; y++){
         for(int x=0; x<width; x++){
-            //for(int channel=0; channel<3; channel++)
-            int channel = 0;
-            {
-                gradX.at<cv::Vec3f>(y,x)[channel] = img.at<cv::Vec3f>(y,x)[channel]; //test
-                //gradX.at<cv::Vec3f>(y,x)[channel] = fabs(img.at<cv::Vec3f>(y, clamp(x+1, 0, width-1))[channel] - 
-                //                                         img.at<cv::Vec3f>(y, clamp(x-1, 0, width-1))[channel]); 
-                //float tmp_gradY = img.at<cv::Vec3f>(y+1,x)[channel] - img.at<cv::Vec3f>(y-1,x)[channel];
+            for(int channel=0; channel<3; channel++){
+                gradX.at<cv::Vec3f>(y,x)[channel] = fabs(img.at<cv::Vec3f>(y, clamp(x+1, 0, width-1))[channel] - 
+                                                         img.at<cv::Vec3f>(y, clamp(x-1, 0, width-1))[channel]);
 
+                gradY.at<cv::Vec3f>(y,x)[channel] = fabs(img.at<cv::Vec3f>(clamp(y+1, 0, height-1), x)[channel] -
+                                                         img.at<cv::Vec3f>(clamp(y-1, 0, height-1), x)[channel]); 
             }
         }
     }
 
     imwrite("gradX_gold.jpg", gradX);
+    imwrite("gradY_gold.jpg", gradY);
 }
 
 
